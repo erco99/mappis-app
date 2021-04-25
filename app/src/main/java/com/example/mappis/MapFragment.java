8f07550ae5a6b78d7dcc7bdfca9143d18d5d4002
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-04-25 10:55:30 +0200

    Create map fragment and implement main life cycle methods

---
+++ app/src/main/java/com/example/mappis/MapFragment.java
@@ -0,0 +1,127 @@
+package com.example.mappis;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.fragment.app.Fragment;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+public class MapFragment extends Fragment implements LocationListener {
+
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private RotationGestureOverlay mRotationGestureOverlay;
+    protected ImageButton btCenterMap;
+    private Location currentLocation = null;
+    private LocationManager lm;
+
+    private MapView mMapView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.map_track, null);
+        mMapView = v.findViewById(R.id.map);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        //super.onViewCreated(view, savedInstanceState)
+
+        final Context context = this.getActivity();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+
+        this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context),
+                mMapView);
+        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context),
+                mMapView);
+
+        mScaleBarOverlay = new ScaleBarOverlay(mMapView);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mRotationGestureOverlay = new RotationGestureOverlay(mMapView);
+        mRotationGestureOverlay.setEnabled(true);
+
+        mMapView.getController().setZoom(15.0);
+        mMapView.setTilesScaledToDpi(true);
+        mMapView.setMultiTouchControls(true);
+        mMapView.setFlingEnabled(true);
+        mMapView.getOverlays().add(this.mLocationOverlay);
+        mMapView.getOverlays().add(this.mCompassOverlay);
+        mMapView.getOverlays().add(this.mScaleBarOverlay);
+
+        mLocationOverlay.enableMyLocation();
+        mLocationOverlay.enableFollowLocation();
+        mLocationOverlay.setOptionsMenuEnabled(true);
+        mCompassOverlay.enableCompass();
+
+        btCenterMap = view.findViewById(R.id.ic_center_map);
+        btCenterMap.setOnClickListener(v -> {
+            if (currentLocation != null) {
+                GeoPoint myPosition = new GeoPoint(currentLocation.getLatitude(), currentLocation.getLongitude());
+                mMapView.getController().animateTo(myPosition);
+            }
+        });
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            lm.removeUpdates(this);
+        } catch (Exception ex){
+        }
+        mCompassOverlay.disableCompass();
+        mLocationOverlay.disableFollowLocation();
+        mLocationOverlay.disableMyLocation();
+        mScaleBarOverlay.enableScaleBar();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        this.currentLocation = location;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        lm = null;
+        currentLocation = null;
+
+        mLocationOverlay = null;
+        mCompassOverlay = null;
+        mScaleBarOverlay = null;
+        mRotationGestureOverlay = null;
+        btCenterMap = null;
+    }
+}
