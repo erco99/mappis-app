548799a9dcafbd4e56f30399018b1f0304671857
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-04-26 14:57:34 +0200

    Create Utilities class and add fragment add method

---
+++ app/src/main/java/com/example/mappis/Utilities.java
@@ -0,0 +1,15 @@
+    package com.example.mappis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class Utilities {
+
+    static void insertFragment(AppCompatActivity activity, Fragment fragment, String tag){
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container_view, fragment, tag);
+        transaction.commit();
+    }
+}
