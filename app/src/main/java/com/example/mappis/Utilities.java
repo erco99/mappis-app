0ee65823612051d340793d78057492c65605e672
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-05-02 09:51:52 +0200

    Add static fields containing the draggable icons

--- app/src/main/java/com/example/mappis/Utilities.java
+++ app/src/main/java/com/example/mappis/Utilities.java
@@ -1,10 +1,17 @@
     package com.example.mappis;
 
+import android.location.Location;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class Utilities {
+
+    static final int[] pencil_type_icons = {R.drawable.pencil, R.drawable.nature};
+    static final int[] drag_icons = {R.drawable.waves, R.drawable.lake};
+    static final int[] text_icons = {};
+
 
     static void insertFragment(AppCompatActivity activity, Fragment fragment, String tag){
         FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
@@ -12,4 +19,5 @@ public class Utilities {
         transaction.replace(R.id.fragment_container_view, fragment, tag);
         transaction.commit();
     }
+
 }
