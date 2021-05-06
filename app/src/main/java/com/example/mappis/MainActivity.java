d23d30e9bfdadeaef01c3c6c0baa7e6cbbcd73e0
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-05-06 15:02:53 +0200

    Add fragment

--- app/src/main/java/com/example/mappis/MainActivity.java
+++ app/src/main/java/com/example/mappis/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_map);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null)
+            Utilities.insertFragment(this, new HomeFragment(), "HomeFragment");
     }
 }
