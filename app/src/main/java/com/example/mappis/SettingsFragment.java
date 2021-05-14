bc1aa6d6e7a940a5edde6c9a1dde6fbcbcb6e3f1
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-05-14 18:05:49 +0200

    Create basic settings fragment

---
+++ app/src/main/java/com/example/mappis/SettingsFragment.java
@@ -0,0 +1,35 @@
+package com.example.mappis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.utils.widget.ImageFilterButton;
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+    ImageFilterButton goBack;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final Activity activity = getActivity();
+
+        goBack = view.findViewById(R.id.goBackButton);
+        goBack.setOnClickListener(v -> {
+            ((AppCompatActivity) activity).getSupportFragmentManager().popBackStack();
+        });
+    }
+}
