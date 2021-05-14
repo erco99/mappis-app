6a287d015760ee6924879e45fba4e3150862b2b8
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-05-14 18:16:11 +0200

    Add static field kmldocument

--- app/src/main/java/com/example/mappis/Utilities.java
+++ app/src/main/java/com/example/mappis/Utilities.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import org.osmdroid.bonuspack.kml.KmlDocument;
+
 public class Utilities {
 
     static final int[] pencil_type_icons = {R.drawable.nature};
     static final int[] drag_icons = {R.drawable.waves, R.drawable.lake};
     static final int[] text_icons = {};
+
+    static KmlDocument kmlDocument = new KmlDocument();
 
 
     static void insertFragment(AppCompatActivity activity, Fragment fragment, String tag){
