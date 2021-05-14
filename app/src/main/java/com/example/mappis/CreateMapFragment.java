d88ef12d9ecd64d672e61e18cd61e2158f5a41c5
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-05-14 18:10:58 +0200

    Add check on fields (blank not permitted)

--- app/src/main/java/com/example/mappis/CreateMapFragment.java
+++ app/src/main/java/com/example/mappis/CreateMapFragment.java
@@ -2,25 +2,32 @@ package com.example.mappis;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.utils.widget.ImageFilterButton;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.example.mappis.CardMaps.AddCardViewModel;
 import com.example.mappis.CardMaps.CardItem;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.IOException;
 
 public class CreateMapFragment extends Fragment {
 
@@ -30,6 +37,7 @@ public class CreateMapFragment extends Fragment {
     AddCardViewModel addCardViewModel;
     ImageFilterButton goBack;
     Spinner spinner;
+    ImageView  mapImage;
 
     @Nullable
     @Override
@@ -45,17 +53,33 @@ public class CreateMapFragment extends Fragment {
 
         mapName = view.findViewById(R.id.mapNameTextInputEditText);
         mapDescription = view.findViewById(R.id.mapDescTextInputEditText);
-        spinner = (Spinner) activity.findViewById(R.id.spinner);
+        spinner = activity.findViewById(R.id.spinner);
+
+
 
         addCardViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(AddCardViewModel.class);
 
+        mapImage = view.findViewById(R.id.mapImageView);
+
         createButton = view.findViewById(R.id.createMapButton);
         createButton.setOnClickListener(v -> {
-            addCardViewModel.addCardItem(new CardItem(mapName.getText().toString(),
-                    mapDescription.getText().toString()));
-            Intent intent = new Intent(activity, MapActivity.class);
-            intent.putExtra("map_type", spinner.getSelectedItem().toString());
-            activity.startActivity(intent);
+
+            if(TextUtils.isEmpty(mapName.getText().toString()) ||
+                    TextUtils.isEmpty(mapDescription.getText().toString())) {
+                Toast.makeText(activity, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+            } else {
+                addCardViewModel.addCardItem(new CardItem(
+                        mapName.getText().toString(),
+                        mapDescription.getText().toString()));
+                Intent intent = new Intent(activity, MapActivity.class);
+                intent.putExtra("map_type", spinner.getSelectedItem().toString());
+
+
+                activity.startActivity(intent);
+                activity.finish();
+
+            }
+
         });
 
         goBack = view.findViewById(R.id.goBackButton);
