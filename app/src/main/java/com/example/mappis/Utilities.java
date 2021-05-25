02f82d247093d7251907b93cc60ae77de568ea4a
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-05-25 15:42:36 +0200

    Change images

--- app/src/main/java/com/example/mappis/Utilities.java
+++ app/src/main/java/com/example/mappis/Utilities.java
@@ -13,10 +13,22 @@ public class Utilities {
     static final int REQUEST_ACCESS_FINE_LOCATION = 1;
     static final int REQUEST_WRITE_EXTERNAL_STORAGE = 2;
 
-    static final int[] pencil_type_icons = {R.drawable.nature};
-    static final int[] drag_icons = {R.drawable.lake128, R.drawable.lake256, R.drawable.lake,
-    R.drawable.marker_default, R.drawable.wavesgood, R.drawable.letter};
-    static final int[] text_icons = {};
+    static final int[] drag_icons = {R.drawable.map_icons_standard_antenna,
+            R.drawable.map_icons_standard_danger, R.drawable.map_icons_standard_fountain,
+            R.drawable.map_icons_standard_house, R.drawable.map_icons_standard_house2,
+            R.drawable.map_icons_standard_lake, R.drawable.map_icons_standard_lake2,
+            R.drawable.map_icons_standard_lakeandtrees, R.drawable.map_icons_standard_river,
+            R.drawable.map_icons_standard_tree, R.drawable.map_icons_standard_tree2,
+            R.drawable.map_icons_standard_tree3, R.drawable.map_icons_standard_tree4,
+            R.drawable.map_icons_standard_tree5, R.drawable.map_icons_standard_trees,
+            R.drawable.map_icons_standard_trees2, R.drawable.map_icons_standard_warning
+    };
+
+    static final int[] text_icons = {R.drawable.map_icons_text_alert,
+            R.drawable.map_icons_text_pin, R.drawable.map_icons_text_pin1,
+            R.drawable.map_icons_text_pin2, R.drawable.map_icons_text_placeholder,
+            R.drawable.map_icons_text_placeholder1, R.drawable.map_icons_text_placeholder2,
+            R.drawable.map_icons_text_placeholder3, R.drawable.map_icons_text_placeholder4};
 
     static KmlDocument kmlDocument = new KmlDocument();
 
